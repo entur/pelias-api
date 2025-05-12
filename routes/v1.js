0e069f049bed9e2c0c4c76e07e28fee28b39c0c8
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-05-12 11:50:36 +0200

    Use latest available api version

--- routes/v1.js
+++ routes/v1.js
@@ -104,7 +104,7 @@ const Interpolation = require('../service/configurations/Interpolation');
  * @param {object} peliasConfig
  */
 function addRoutes(app, peliasConfig) {
-  const esclient = elasticsearch.Client(Object.assign({}, peliasConfig.esclient, { apiVersion: '5.6' }));
+  const esclient = elasticsearch.Client(Object.assign({}, peliasConfig.esclient, { apiVersion: '7.6' }));
 
   const pipConfiguration = new PointInPolygon(_.defaultTo(peliasConfig.api.services.pip, {}));
   const pipService = serviceWrapper(pipConfiguration);
